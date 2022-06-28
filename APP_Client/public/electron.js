444c22f6cef8455e4ea49986aff7a8ffab5d1aa4
sthouvenin67 <sebastien.thouvenin@efrei.net>
2022-06-28 15:13:51 +0200

    no more resizing

--- APP_Client/public/electron.js
+++ APP_Client/public/electron.js
@@ -13,7 +13,7 @@ function createWindow() {
         width: 900, 
         height: 600,
         icon: "",
-        //resizable: false,
+        resizable: false,
         minHeight: 500,
         minWidth: 750,
 
