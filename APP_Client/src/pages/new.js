1f682ab1906ec4609b3da11f62c948e859bb1144
sthouvenin67 <sebastien.thouvenin@efrei.net>
2022-06-29 09:23:05 +0200

    New message css done

--- APP_Client/src/pages/new.js
+++ APP_Client/src/pages/new.js
@@ -53,24 +53,24 @@ const New = () => {
         <div className="new-mess-column">
             <div className="line-to">
                 <div className="labeltext">
-                    TO :
+                    &#192; :
                 </div>
                 <div>
-                    <input className="new_input" type="text" id="to" placeholder="pseudo du destinataire" />
+                    <input className="new_input" type="text" id="to" placeholder="Destinataire" />
                 </div>
             </div>
             <div className="line-to">
                 <div className="labeltext">
-                    Attachement :
+                    Pi&#232;ce jointe :
                 </div>
                 <div>
-                    <input className="new_input" type="file" id="attachement" placeholder="Attachement" />
+                    <input className="new_input" type="file" id="attachement" />
                 </div>
             </div>
             <div className="content-message">
-                <textarea className="content-message-area" id="content-message-id" placeholder="Votre message" />
+                <textarea className="content-message-area" id="content-message-id" placeholder=" Votre message" />
             </div>
-            <div className="div-bouton">
+            <div className="new-bouton">
                 <button className="button_envoyer">Envoyer</button>
             </div>
         </div>
